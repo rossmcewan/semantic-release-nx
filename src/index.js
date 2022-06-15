e47cf10486a7f9cf26fd26bd64e51acb9c48f938
Ross McEwan <ross.mcewan@snyk.io>
2022-06-15 12:31:45 +0200

    fix: invalid import

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
-const readPkg = require('read-pkg');
-const { compose } = require('ramda');
-const withOnlyPackageCommits = require('./only-package-commits');
-const versionToGitTag = require('./version-to-git-tag');
-const logPluginVersion = require('./log-plugin-version');
-const { wrapStep } = require('semantic-release-plugin-decorators');
+const readPkg = require("read-pkg");
+const { compose } = require("ramda");
+const { withOnlyPackageCommits } = require("./only-package-commits");
+const versionToGitTag = require("./version-to-git-tag");
+const logPluginVersion = require("./log-plugin-version");
+const { wrapStep } = require("semantic-release-plugin-decorators");
 
 const {
   mapNextReleaseVersion,
   withOptionsTransforms,
-} = require('./options-transforms');
+} = require("./options-transforms");
 
 const analyzeCommits = wrapStep(
-  'analyzeCommits',
-  compose(logPluginVersion('analyzeCommits'), withOnlyPackageCommits),
+  "analyzeCommits",
+  compose(logPluginVersion("analyzeCommits"), withOnlyPackageCommits),
   {
-    wrapperName: 'semantic-release-monorepo',
+    wrapperName: "semantic-release-monorepo",
   }
 );
 
 const generateNotes = wrapStep(
-  'generateNotes',
+  "generateNotes",
   compose(
-    logPluginVersion('generateNotes'),
+    logPluginVersion("generateNotes"),
     withOnlyPackageCommits,
     withOptionsTransforms([mapNextReleaseVersion(versionToGitTag)])
   ),
   {
-    wrapperName: 'semantic-release-monorepo',
+    wrapperName: "semantic-release-monorepo",
   }
 );
 
 const success = wrapStep(
-  'success',
+  "success",
   compose(
-    logPluginVersion('success'),
+    logPluginVersion("success"),
     withOnlyPackageCommits,
     withOptionsTransforms([mapNextReleaseVersion(versionToGitTag)])
   ),
   {
-    wrapperName: 'semantic-release-monorepo',
+    wrapperName: "semantic-release-monorepo",
   }
 );
 
 const fail = wrapStep(
-  'fail',
+  "fail",
   compose(
-    logPluginVersion('fail'),
+    logPluginVersion("fail"),
     withOnlyPackageCommits,
     withOptionsTransforms([mapNextReleaseVersion(versionToGitTag)])
   ),
   {
-    wrapperName: 'semantic-release-monorepo',
+    wrapperName: "semantic-release-monorepo",
   }
 );
 
@@ -59,5 +59,5 @@ module.exports = {
   generateNotes,
   success,
   fail,
-  tagFormat: readPkg.sync().name + '-v${version}',
+  tagFormat: readPkg.sync().name + "-v${version}",
 };
